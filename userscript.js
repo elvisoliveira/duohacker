4e690c8a6d5c662d145629a10f1ff374bcefe539
Elvis Oliveira <elvis.olv@gmail.com>
2023-08-08 22:04:46 +0200

    I thought you were upset with me

--- userscript.js
+++ userscript.js
@@ -12,6 +12,8 @@
 // @match            https://www.duolingo.com/checkpoint*
 // @match            https://www.duolingo.com/stories*
 // @match            https://www.duolingo.com/practice*
+// @match            https://www.duolingo.com/lesson*
+// @match            https://www.duolingo.com/learn*
 // @license          MIT
 // @grant            none
 // @run-at           document-end
@@ -21,10 +23,10 @@
 // @author smintf
 // ==/OpenUserJS==
 
-const DEBUG = false;
+const DEBUG = true;
 let mainInterval;
-const dataTestComponentClassName = ".e4VJZ, .FQpeZ";
-const TIME_OUT = 1000;
+const dataTestComponentClassName = ".e4VJZ, .FQpeZ, ._35e5D";
+const TIME_OUT = 3000;
 
 // Challenge types
 const CHARACTER_SELECT_TYPE = "characterSelect";
@@ -73,11 +75,11 @@ const CHALLENGE_LISTEN_TAP = '[data-test="challenge-listenTap"]';
 const CHALLENGE_LISTEN_SPELL = '[data-test="challenge challenge-listenSpell"]';
 const CHALLENGE_JUDGE_TEXT = '[data-test="challenge-judge-text"]';
 const CHALLENGE_TEXT_INPUT = '[data-test="challenge-text-input"]';
-const CHALLENGE_TAP_TOKEN = '[data-test="challenge-tap-token"]';
+const CHALLENGE_TAP_TOKEN = '[data-test*="challenge-tap-token"]';
 const CHALLENGE_TAP_TOKEN_TEXT = '[data-test="challenge-tap-token-text"]';
 const CHALLENGE_TYPE_CLOZE = '[data-test="challenge challenge-typeCloze"]';
 const CHALLENGE_TYPE_CLOZE_TABLE = '[data-test="challenge challenge-typeClozeTable"]';
-const PLAYER_NEXT = '[data-test="player-next"]';
+const PLAYER_NEXT = '[data-test="player-next"], [data-test="story-start"]';
 const PLAYER_SKIP = '[data-test="player-skip"]';
 const AUDIO_BUTTON = '[data-test="audio-button"]';
 const WORD_BANK = '[data-test="word-bank"]';
@@ -85,6 +87,8 @@ const BLAME_INCORRECT = '[data-test="blame blame-incorrect"]';
 const CHARACTER_MATCH = '[data-test="challenge challenge-characterMatch"]';
 const TYPE_COMPLETE_TABLE = '[data-test="challenge challenge-typeCompleteTable"]';
 const STORIES_PLAYER_NEXT = '[data-test="stories-player-continue"]';
+const STORIES_PLAYER_START = '[data-test="story-start"]';
+const STORIES_PLAYER_DONE = '[data-test="stories-player-done"]';
 const STORIES_CHOICE = '[data-test="stories-choice"]';
 const STORIES_ELEMENT = '[data-test="stories-element"]';
 
@@ -126,21 +130,56 @@ function getChallengeObj(theObject) {
   return result;
 }
 
+function getElementIndex(element) {
+  let result = null;
+  if (element instanceof Array) {
+    for (let i = 0; i < element.length; i++) {
+      result = getElementIndex(element[i]);
+      if (result) {
+        break;
+      }
+    }
+  } else {
+    for (let prop in element) {
+      // if (prop == "elementIndex") {
+      if (prop == "challenge") {
+        if (typeof element[prop] == "object") {
+          return element;
+        }
+        return element[prop];
+      }
+      if (element[prop] instanceof Object || element[prop] instanceof Array) {
+        result = getElementIndex(element[prop]);
+        if (result) {
+          break;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+function getProps(element) {
+  let propsClass = Object.keys(element).filter((att) => /^__reactProps/g.test(att))[0];
+  return element[propsClass];
+}
+
 // Gets the Challenge
 function getChallenge() {
-  const dataTestDOM = document.querySelectorAll(
-    dataTestComponentClassName
-  )[0];
-
-  if (!dataTestDOM) {
-    document.querySelectorAll(PLAYER_NEXT)[0].dispatchEvent(clickEvent);
-    return null;
-  } else {
-    const dataTestAtrr = Object.keys(dataTestDOM).filter((att) =>
-      /^__reactProps/g.test(att)
-    )[0];
-    const childDataTestProps = dataTestDOM[dataTestAtrr];
-    const { challenge } = getChallengeObj(childDataTestProps);
+  const dataTestDOM = document.querySelectorAll(dataTestComponentClassName);
+  if (dataTestDOM.length > 0) {
+    let current = 0;
+    for (let i = 0; i < dataTestDOM.length; i++) {
+      if(dataTestDOM[i].childNodes.length > 0) {
+        current = i;
+      }
+    }
+    let currentDOM = dataTestDOM[current];
+    let propsValues = getProps(currentDOM);
+    const { challenge } = getElementIndex(propsValues);
+    // let elementIndex = getElementIndex(propsValues);
+    // let storyList = getProps(currentDOM.parentNode);
+    // return storyList.children.find(Boolean)[elementIndex].props.storyElement;
     return challenge;
   }
 }
@@ -165,9 +204,10 @@ function pressEnter() {
     cancelable: false,
   });
   breakWhenIncorrect();
-  document
-    .querySelector('button[data-test="player-next"]')
-    .dispatchEvent(clickEvent);
+  let isPlayerNext = document.querySelector(PLAYER_NEXT);
+  if (isPlayerNext !== null) {
+    isPlayerNext.dispatchEvent(clickEvent);
+  }
 }
 
 // pressEnter() function but for stories
@@ -177,9 +217,7 @@ function pressEnterStories() {
     bubbles: true,
     cancelable: false,
   });
-  document
-    .querySelector('button[data-test="stories-player-continue"]')
-    .dispatchEvent(clickEvent);
+  document.querySelector([STORIES_PLAYER_NEXT, STORIES_PLAYER_START, STORIES_PLAYER_DONE].join(',')).dispatchEvent(clickEvent);
 }
 
 // dynamicInput() function
@@ -202,6 +240,57 @@ function classify() {
   if (!challenge) return;
   if (DEBUG) terminal.log(`${challenge.type}`, challenge);
   switch (challenge.type) {
+    case 'MULTIPLE_CHOICE':
+    case 'SELECT_PHRASE': {
+      const { correctAnswerIndex } = challenge;
+      if (DEBUG) { terminal.log("MULTIPLE_CHOICE_TYPE, SELECT_PHRASE", { correctAnswerIndex }); }
+      const options = document.querySelectorAll('[data-test="stories-choice"]');
+      options.forEach((option, i) => {
+        if(i == correctAnswerIndex) {
+          option.dispatchEvent(clickEvent);
+        }
+      });
+      return { correctAnswerIndex };
+    }
+
+    case 'POINT_TO_PHRASE': {
+      const { correctAnswerIndex } = challenge;
+      if (DEBUG) { terminal.log("POINT_TO_PHRASE", { correctAnswerIndex }); }
+      const options = document.querySelectorAll('[data-test="challenge-tap-token"]');
+      options.forEach((option, i) => {
+        if(i == correctAnswerIndex) {
+          option.dispatchEvent(clickEvent);
+        }
+      });
+      return { correctAnswerIndex };
+    }
+
+    case 'ARRANGE': {
+      const { selectablePhrases, phraseOrder } = challenge;
+      if (DEBUG) { terminal.log("POINT_TO_PHRASE", { selectablePhrases, phraseOrder }); }
+      const options = document.querySelectorAll('[data-test="challenge-tap-token"]');
+      if(options.length > 0) {
+        phraseOrder.forEach((i) => {
+            Array.prototype.slice.call(options).filter(function(option) { return option.innerText == selectablePhrases[i] }).find(Boolean).dispatchEvent(clickEvent);
+        });
+      }
+      return { selectablePhrases, phraseOrder };
+    }
+
+    case 'MATCH': {
+      const { matches } = challenge;
+      if (DEBUG) { terminal.log("MATCH", { matches }); }
+      const options = document.querySelectorAll('[data-test="challenge-tap-token"]');
+      matches.forEach((match) => {
+        options.forEach((option) => {
+          if(option.querySelector('[data-test="challenge-tap-token-text"]').innerText == match.phrase || option.querySelector('[data-test="challenge-tap-token-text"]').innerText == match.translation) {
+            option.dispatchEvent(clickEvent);
+          }
+        });
+      });
+      return { matches };
+    }
+
     case LISTEN_SPELL_TYPE: {
       const { displayTokens } = challenge;
       if (DEBUG) { terminal.log("LISTEN_SPELL_TYPE", { displayTokens }); }
@@ -243,21 +332,23 @@ function classify() {
             value = value + token.text;
         }
       });
-      tokens[0].textContent = value;
-      tokens[0].dispatchEvent(event);
-      return { displayTokens, grader  };
+      try {
+        tokens[0].textContent = value;
+        tokens[0].dispatchEvent(event);
+      } catch (e) { /* terminal.log(e); */ }
+      return { displayTokens, grader };
     }
 
     case LISTEN_MATCH_TYPE: {
       const { pairs } = challenge;
       if (DEBUG) { terminal.log("LISTEN_MATCH_TYPE", { pairs }); }
-      const tokens = document.querySelectorAll(CHALLENGE_TAP_TOKEN);
+      const tokens = document.querySelectorAll('button'.concat(CHALLENGE_TAP_TOKEN));
       for (let i = 0; i <= 3; i++) {
         f = getReactFiber(tokens[i]);
-        text = f.return.return.memoizedProps.text;
+        text = f.return.return.memoizedProps.children.props["data-test"].split("-")[0];
         tokens[i].dispatchEvent(clickEvent);
         for (let j = 4; j <= 7; j++) {
-          if(tokens[j].querySelector(CHALLENGE_TAP_TOKEN_TEXT).innerText == text) {
+          if(tokens[j].querySelector(CHALLENGE_TAP_TOKEN_TEXT).innerText.includes(text)) {
             tokens[j].dispatchEvent(clickEvent);
           }
         }
@@ -403,7 +494,7 @@ function classify() {
 
     case TAP_COMPLETE_TYPE: {
       const { choices, correctIndices } = challenge;
-      const tokens = document.querySelectorAll(WORD_BANK.concat(' ', CHALLENGE_TAP_TOKEN));
+      const tokens = document.querySelectorAll(WORD_BANK.concat(' ', CHALLENGE_TAP_TOKEN_TEXT));
       if (DEBUG) {terminal.log("TAP_COMPLETE_TYPE", { choices, correctIndices, tokens });}
       correctIndices.forEach((i) => {
         tokens[i].dispatchEvent(clickEvent);
@@ -461,7 +552,7 @@ function classify() {
       const { correctTokens, correctSolutions } = challenge;
       if (DEBUG) {terminal.log("TRANSLATE_TYPE", { correctTokens, correctSolutions });}
       if (correctTokens) {
-        const tokens = document.querySelectorAll(CHALLENGE_TAP_TOKEN);
+        const tokens = document.querySelectorAll(CHALLENGE_TAP_TOKEN_TEXT);
         let ignoreTokeIndexes = [];
         for (let correctTokenIndex in correctTokens) {
           for (let tokenIndex in tokens) {
@@ -608,14 +699,16 @@ function breakWhenIncorrect() {
 // Main Function
 function main() {
   try {
-    let isPlayerNext = document
-      .querySelectorAll(PLAYER_NEXT)[0]
-      .textContent.toUpperCase();
-    if (isPlayerNext.valueOf() !== "CONTINUE") {
+    let isPlayerNext = document.querySelectorAll(PLAYER_NEXT);
+    let test = document.querySelector("[data-test=\"plus-no-thanks\"], [data-test=\"practice-hub-ad-no-thanks-button\"]");
+    if (isPlayerNext !== null && isPlayerNext.length > 0 && isPlayerNext[0].textContent.toUpperCase().valueOf() !== "CONTINUE") {
       classify();
-      // pressEnter();
+    } else if (test !== null && test.length > 0) {
+      test.click();
+    } else if (/learn/gi.test(window.location.href) == true) {
+      learn();
     }
-    setTimeout(pressEnter, 150);
+    setTimeout(pressEnter, 150); // pressEnter();
   } catch (e) {
     // terminal.log(e);
   }
@@ -623,15 +716,34 @@ function main() {
 
 // Stories Function
 function stories() {
+  if (/stories/gi.test(window.location.href) == false) {
+    window.location.replace("https://www.duolingo.com/stories"); return;
+  }
   try {
-    let isPlayerNext = document
-      .querySelectorAll(STORIES_PLAYER_NEXT)[0]
-      .textContent.toUpperCase();
-    if (isPlayerNext.valueOf() !== "CONTINUE") {
-      classify();
-      pressEnterStories();
+    let button = document.querySelectorAll([STORIES_PLAYER_NEXT, STORIES_PLAYER_START, STORIES_PLAYER_DONE].join(','));
+    if(button.length > 0) {
+      let action = Array.prototype.slice.call(button).find(Boolean).textContent.toUpperCase();
+      if (action.valueOf() == "START STORY") {
+        setTimeout(pressEnterStories, 150);
+      } else if (action.valueOf() == "CONTINUE") {
+        if(Array.prototype.slice.call(button).find(Boolean).disabled) {
+          classify();
+        } else {
+          setTimeout(pressEnterStories, 150);
+        }
+      }
+    } else {
+      let button = document.querySelector('[data-test="story-start-button"]');
+      if(button) {
+        if(button.nextSibling.disabled) {
+          button.dispatchEvent(clickEvent);
+        } else {
+          button.nextSibling.dispatchEvent(clickEvent);
+        }
+      } else {
+        document.querySelector('div._2zY7s:not([style*="255, 177, 0"]):not([style*="251, 177, 0"])').dispatchEvent(clickEvent);
+      }
     }
-    setTimeout(pressEnterStories, 50);
   } catch (e) {
     terminal.log(e);
   }
@@ -639,26 +751,47 @@ function stories() {
 
 // Calls main()
 function solveChallenge() {
-  setConsole();
+  if(document.getElementById('mylog') == null) {
+    setConsole();
+  }
   // Check if its a Skill / Alphabet / Checkpoint URL
-  if (/[sac][klh][ipe][lhc][lak]/gi.test(window.location.href) == true) {
+  // if (/[sac][klh][ipe][lhc][lak]/gi.test(window.location.href) == true) {
+  if (/lesson|practice/gi.test(window.location.href) == true) {
     if (DEBUG) {terminal.log("Skill URL Detected");}
+    clearInterval(mainInterval);
     mainInterval = setInterval(main, TIME_OUT);
   }
   // Check if its a Stories URL
   if (/stories/gi.test(window.location.href) == true) {
     if (DEBUG) {terminal.log("Stories URL Detected");}
+    clearInterval(mainInterval);
     mainInterval = setInterval(stories, TIME_OUT);
   }
+  if (/learn/gi.test(window.location.href) == true) {
+    if (DEBUG) {terminal.log("Main URL Detected");}
+    clearInterval(mainInterval);
+    mainInterval = setInterval(learn, TIME_OUT);
+  }
   terminal.log(`to stop the script run "clearInterval(${mainInterval})"`);
+}
+
+function learn() {
+  let button = document.querySelector('a[data-test="global-practice"]');
+  if(button) {
+      button.click();
+  }
+  clearInterval(mainInterval);
+  mainInterval = setInterval(solveChallenge, TIME_OUT);
 }
 
 // To not mess duolingo own log
 function setConsole() {
   var iframe = document.createElement('iframe');
+  iframe.id = 'mylog';
   iframe.style.display = 'none';
   document.body.appendChild(iframe);
   window.terminal = iframe.contentWindow.console;
 }
 
 (solveChallenge)();
+
